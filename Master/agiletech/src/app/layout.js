8718190dd2e7e6ad5894172900d67d33a059de63
Amit Biswas <67472157+amitbd599@users.noreply.github.com>
2024-06-20 14:33:17 +0600

    add client file

--- Master/agiletech/src/app/layout.js
+++ Master/agiletech/src/app/layout.js
@@ -4,7 +4,7 @@ import "./font.css";
 import "./globals.scss";
 
 export const metadata = {
-  title: "Service || AglieTech - IT Solutions & Technology NEXT JS Template",
+  title: "Demo || AglieTech - IT Solutions & Technology NEXT JS Template",
   description:
     "Agiletech provide you to build the best agency, app, business, digital, it services, it solutions, network solution, startup, technology, technology company, technology service template.",
 };
--- Master/agiletech/src/app/page.js
+++ Master/agiletech/src/app/page.js
@@ -2,7 +2,7 @@ import Demo from "@/demo/Demo";
 
 
 export const metadata = {
-  title: "Service || AglieTech - IT Solutions & Technology NEXT JS Template",
+  title: "Demo || AglieTech - IT Solutions & Technology NEXT JS Template",
   description:
     "Agiletech provide you to build the best agency, app, business, digital, it services, it solutions, network solution, startup, technology, technology company, technology service template.",
 };
